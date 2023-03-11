ab036371f3b9d22da2c9b86b0648aa3361a2b3ca
29144 <2914411787@qq.com>
2023-03-11 20:06:12 +0800

    热部署

--- src/main/java/com/juguopeng/endgame/EndGameApplication.java
+++ src/main/java/com/juguopeng/endgame/EndGameApplication.java
@@ -1,9 +1,12 @@
 package com.juguopeng.endgame;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.Mapping;
 
 @SpringBootApplication
+@MapperScan("com.juguopeng.endgame.mapper")//扫描路径下的包，编译后生成相应的接口类
 public class EndGameApplication {
 
     public static void main(String[] args) {
