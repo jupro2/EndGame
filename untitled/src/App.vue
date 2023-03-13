aa8380f3e39682108a1e086f1b55f733c37689d5
29144 <2914411787@qq.com>
2023-03-13 19:16:25 +0800

    更新

---
+++ untitled/src/App.vue
@@ -0,0 +1,33 @@
+<script>
+import Carousel from "@/components/icons/Carousel.vue";
+import Menu from "@/components/icons/Menu.vue";
+export default {
+  name:"home",
+  components:{
+    Carousel,
+    Menu,
+  }
+}
+</script>
+
+<template>
+<!--  菜单-->
+  <div>
+    <Menu/>
+    <!--  走马灯-->
+  </div>
+  <div>
+    <Carousel/>
+  </div>
+
+  <main>
+
+  </main>
+</template>
+
+<style>
+
+
+
+
+</style>
---
+++ untitled/src/main.js
@@ -0,0 +1,10 @@
+//main.js
+import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
+import App from './App.vue'
+import 'element-plus/dist/index.css'
+
+const app = createApp(App)
+
+app.use(ElementPlus)
+app.mount('#app')
